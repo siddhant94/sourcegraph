1a1f068743d6b5a46cb7a377b22eeac2e6360c38
Rijnard van Tonder <rvantonder@gmail.com>
2021-10-28 19:14:38 -0700

    compute: add regexp substitution to output command

--- internal/compute/output_command.go
+++ internal/compute/output_command.go
@@ -3,6 +3,8 @@ package compute
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/sourcegraph/sourcegraph/internal/search/result"
 )
@@ -17,6 +19,30 @@ func (c *Output) String() string {
 	return fmt.Sprintf("Output with separator: (%s) -> (%s) separator: %s", c.MatchPattern.String(), c.OutputPattern, c.Separator)
 }
 
-func (c *Output) Run(context.Context, *result.FileMatch) (Result, error) {
-	return nil, nil
+func substituteRegexp(content string, match *regexp.Regexp, replacePattern, separator string) string {
+	result := []byte{}
+	for _, submatches := range match.FindAllStringSubmatchIndex(content, -1) {
+		result = append(match.ExpandString(result, replacePattern, content, submatches), separator...)
+	}
+	return string(result)
+}
+
+func output(ctx context.Context, fragment []byte, matchPattern MatchPattern, replacePattern string, separator string) (*Text, error) {
+	var newFragment string
+	switch match := matchPattern.(type) {
+	case *Regexp:
+		newFragment = substituteRegexp(string(fragment), match.Value, replacePattern, separator)
+	case *Comby:
+		return nil, nil
+	}
+	return &Text{Value: newFragment, Kind: "output"}, nil
+}
+
+func (c *Output) Run(ctx context.Context, fm *result.FileMatch) (Result, error) {
+	var lines []string
+	for _, line := range fm.LineMatches {
+		lines = append(lines, line.Preview)
+	}
+	fragment := strings.Join(lines, "\n")
+	return output(ctx, []byte(fragment), c.MatchPattern, c.OutputPattern, c.Separator)
 }
